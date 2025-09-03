06d3a2f4e4786144a4dd73eeb636d0f93755b098
pranavpatil-idx <pranav.patil@indexnine.com>
2025-09-03 10:45:18 +0530

    JavaScript Files Added

---
+++ JS Exercises/javascript.js
@@ -0,0 +1,25 @@
+// console.log("Hello World!");
+
+// variables
+
+// let name = 'Pranav';
+// let surname = 'Patil';
+
+// name = 'Sujay';
+
+// console.log(name);
+
+// console.log(surname);
+
+// const pi = 3.14;
+// pi = 10;
+
+// console.log(pi);
+
+// var a = "Hello";
+
+// a = "Namaste";
+
+// console.log(a);
+
+// console.log( (3+2) - 76 * (1+1) );
